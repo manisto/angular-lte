05db7aebd4483d8c2877f184529580dfc9ca16b6
Gert Peter Rue Brigsted <gepeb@odense.dk>
2017-06-19 10:57:50 +0200

    Removed unused AnimateService.

--- src/angular-lte.module.ts
+++ src/angular-lte.module.ts
@@ -1,9 +1,7 @@
 import {componentsModule} from './components/components.module';
 import {directivesModule} from './directives/directives.module';
-import {servicesModule} from './services/services.module';
 
 export let angularLteModule = angular.module('angular-lte', [
     componentsModule.name,
     directivesModule.name,
-    servicesModule.name,
 ]);
--- src/services/services.module.ts
+++
@@ -1,5 +0,0 @@
-import {AnimateService} from './AnimateService';
-
-export let servicesModule = angular.module('angular-lte.services', []);
-
-servicesModule.service('lteAnimateService', AnimateService);
